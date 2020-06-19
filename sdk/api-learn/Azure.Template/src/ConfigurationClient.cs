3be121b395dc10f672e66578af375db24ab08e07
Anne Loomis Thompson <annelo@microsoft.com>
2020-06-18 17:16:46 -0700

    Add client

---
+++ sdk/api-learn/Azure.Template/src/ConfigurationClient.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Learn.AppConfig.Models;
+
+namespace Azure.Learn.AppConfig
+{
+    public class ConfigurationClient
+    {
+        protected ConfigurationClient()
+        {
+        }
+
+        public ConfigurationClient(Uri endpoint, TokenCredential credential) : this(endpoint, credential, new ConfigurationClientOptions())
+        {
+        }
+
+#pragma warning disable CA1801
+        public ConfigurationClient(Uri endpoint, TokenCredential credential, ConfigurationClientOptions options)
+        {
+        }
+#pragma warning restore CA1801
+
+        public virtual Response<KeyValue> GetKeyValue(string key, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<Response<KeyValue>> GetKeyValueAsync(string key, CancellationToken cancellationToken = default)
+        {
+            await Task.Run(() => {}).ConfigureAwait(false);
+            throw new NotImplementedException();
+        }
+    }
+}
