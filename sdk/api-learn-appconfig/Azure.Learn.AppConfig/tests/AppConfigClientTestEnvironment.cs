a60ed028f2d792ffc6661a844e9119ae607eeb43
Anne Loomis Thompson <annelo@microsoft.com>
2020-08-09 14:12:47 -0700

    add sample

---
+++ sdk/api-learn-appconfig/Azure.Learn.AppConfig/tests/AppConfigClientTestEnvironment.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.Template.Tests
+{
+    public class AppConfigClientTestEnvironment : TestEnvironment
+    {
+        public AppConfigClientTestEnvironment() : base("template")
+        {
+        }
+    }
+}
---
+++ sdk/api-learn-appconfig/Azure.Learn.AppConfig/tests/Samples/Sample1_GetConfigurationSetting.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core.TestFramework;
+using Azure.Identity;
+using Azure.Learn.AppConfig;
+using Azure.Learn.AppConfig.Models;
+using NUnit.Framework;
+
+namespace Azure.Template.Tests.Samples
+{
+    public class Sample1_GetConfigurationSetting : SamplesBase<AppConfigClientTestEnvironment>
+    {
+        [Test]
+        public void GetConfigurationSetting()
+        {
+            string endpoint = "http://example.azconfig.io";
+
+            ServiceClient client = new ServiceClient(endpoint, new DefaultAzureCredential());
+
+            KeyValue color = client.GetKeyValue("FontColor");
+            KeyValue greeting = client.GetKeyValue("GreetingText");
+
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color.Value);
+            Console.WriteLine(greeting.Value);
+        }
+    }
+}
