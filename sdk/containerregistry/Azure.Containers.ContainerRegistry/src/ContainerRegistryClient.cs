ac2ddfc0ee0b03a96aa9f3073b687af97a72705e
Anne Loomis Thompson <annelo@microsoft.com>
2021-03-24 14:19:15 -0700

    misc

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/src/ContainerRegistryClient.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/ContainerRegistryClient.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Specialized;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Web;
 
 using Azure.Core;
@@ -17,33 +16,34 @@ namespace Azure.Containers.ContainerRegistry
     {
         private readonly Uri _endpoint;
         private readonly HttpPipeline _pipeline;
+        private readonly HttpPipeline _acrAuthPipeline;
         private readonly ClientDiagnostics _clientDiagnostics;
         private readonly ContainerRegistryRestClient _restClient;
 
+        private readonly AuthenticationRestClient _acrAuthClient;
+        private readonly string AcrAadScope = "https://management.core.windows.net/.default";
+
         /// <summary>
-        /// <paramref name="endpoint"/>
         /// </summary>
-        public ContainerRegistryClient(Uri endpoint, string username, string password) : this(endpoint, username, password, new ContainerRegistryClientOptions())
+        public ContainerRegistryClient(Uri endpoint, TokenCredential credential) : this(endpoint, credential, new ContainerRegistryClientOptions())
         {
         }
 
         /// <summary>
         /// </summary>
-        /// <param name="endpoint"></param>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
-        /// <param name="options"></param>
-        public ContainerRegistryClient(Uri endpoint, string username, string password, ContainerRegistryClientOptions options)
+        public ContainerRegistryClient(Uri endpoint, TokenCredential credential, ContainerRegistryClientOptions options)
         {
             Argument.AssertNotNull(endpoint, nameof(endpoint));
+            Argument.AssertNotNull(credential, nameof(credential));
             Argument.AssertNotNull(options, nameof(options));
 
-            _pipeline = HttpPipelineBuilder.Build(options, new BasicAuthenticationPolicy(username, password));
-
+            _endpoint = endpoint;
             _clientDiagnostics = new ClientDiagnostics(options);
 
-            _endpoint = endpoint;
+            _acrAuthPipeline = HttpPipelineBuilder.Build(options);
+            _acrAuthClient = new AuthenticationRestClient(_clientDiagnostics, _acrAuthPipeline, endpoint.AbsoluteUri);
 
+            _pipeline = HttpPipelineBuilder.Build(options, new ContainerRegistryChallengeAuthenticationPolicy(credential, AcrAadScope, _acrAuthClient));
             _restClient = new ContainerRegistryRestClient(_clientDiagnostics, _pipeline, _endpoint.AbsoluteUri);
         }
 
@@ -56,102 +56,35 @@ namespace Azure.Containers.ContainerRegistry
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         public virtual AsyncPageable<string> GetRepositoriesAsync(CancellationToken cancellationToken = default)
         {
-            async Task<Page<string>> FirstPageFunc(int? pageSizeHint)
+            return PageResponseEnumerator.CreateAsyncEnumerable(async (continuationToken, pageSizeHint) =>
             {
-                using var scope = _clientDiagnostics.CreateScope("ContainerRegistryClient.GetRepositories");
+                using DiagnosticScope scope = _clientDiagnostics.CreateScope($"{nameof(ContainerRegistryClient)}.{nameof(GetRepositories)}");
                 scope.Start();
                 try
                 {
-                    ResponseWithHeaders<Repositories, ContainerRegistryGetRepositoriesHeaders> response = await _restClient.GetRepositoriesAsync(last: null, pageSizeHint, cancellationToken).ConfigureAwait(false);
-                    return Page.FromValues(response.Value.RepositoriesValue, response.Headers.Link, response.GetRawResponse());
+                    Response<Repositories> response =
+                        await _restClient.GetRepositoriesAsync(
+                            continuationToken,
+                            pageSizeHint,
+                            cancellationToken)
+                       .ConfigureAwait(false);
+
+                    string lastRepository = null;
+                    if (!string.IsNullOrEmpty(response.Value.Link))
+                    {
+                        Uri nextLink = new Uri(response.Value.Link);
+                        NameValueCollection queryParams = HttpUtility.ParseQueryString(nextLink.Query);
+                        lastRepository = queryParams["last"];
+                    }
+
+                    return Page<string>.FromValues(response.Value.RepositoriesValue, lastRepository, response.GetRawResponse());
                 }
-                catch (Exception e)
+                catch (Exception ex)
                 {
-                    scope.Failed(e);
+                    scope.Failed(ex);
                     throw;
                 }
-            }
-            async Task<Page<string>> NextPageFunc(string continuationToken, int? pageSizeHint)
-            {
-                using var scope = _clientDiagnostics.CreateScope("ContainerRegistryClient.GetRepositories");
-                scope.Start();
-                try
-                {
-                    ResponseWithHeaders<Repositories, ContainerRegistryGetRepositoriesHeaders> response = await _restClient.GetRepositoriesNextPageAsync(null, continuationToken, pageSizeHint, cancellationToken).ConfigureAwait(false);
-                    return Page.FromValues(response.Value.RepositoriesValue, response.Headers.Link, response.GetRawResponse());
-                }
-                catch (Exception e)
-                {
-                    scope.Failed(e);
-                    throw;
-                }
-            }
-            return PageableHelpers.CreateAsyncEnumerable(FirstPageFunc, NextPageFunc);
-
-            //public virtual AsyncPageable<string> GetRepositoriesAsync(string last = null, int? n = null, CancellationToken cancellationToken = default)
-            //{
-            //    async Task<Page<string>> FirstPageFunc(int? pageSizeHint)
-            //    {
-            //        using var scope = _clientDiagnostics.CreateScope("ContainerRegistryClient.GetRepositories");
-            //        scope.Start();
-            //        try
-            //        {
-            //            var response = await RestClient.GetRepositoriesAsync(last, n, cancellationToken).ConfigureAwait(false);
-            //            return Page.FromValues(response.Value.RepositoriesValue, response.Value.Link, response.GetRawResponse());
-            //        }
-            //        catch (Exception e)
-            //        {
-            //            scope.Failed(e);
-            //            throw;
-            //        }
-            //    }
-            //    async Task<Page<string>> NextPageFunc(string nextLink, int? pageSizeHint)
-            //    {
-            //        using var scope = _clientDiagnostics.CreateScope("ContainerRegistryClient.GetRepositories");
-            //        scope.Start();
-            //        try
-            //        {
-            //            var response = await RestClient.GetRepositoriesNextPageAsync(nextLink, last, n, cancellationToken).ConfigureAwait(false);
-            //            return Page.FromValues(response.Value.RepositoriesValue, response.Value.Link, response.GetRawResponse());
-            //        }
-            //        catch (Exception e)
-            //        {
-            //            scope.Failed(e);
-            //            throw;
-            //        }
-            //    }
-            //    return PageableHelpers.CreateAsyncEnumerable(FirstPageFunc, NextPageFunc);
-            //}
-
-            //return PageResponseEnumerator.CreateAsyncEnumerable(async (continuationToken, pageSizeHint) =>
-            //{
-            //    using DiagnosticScope scope = _clientDiagnostics.CreateScope($"{nameof(ContainerRegistryClient)}.{nameof(GetRepositories)}");
-            //    scope.Start();
-            //    try
-            //    {
-            //        ResponseWithHeaders<Repositories, ContainerRegistryGetRepositoriesHeaders> response =
-            //            await _restClient.GetRepositoriesAsync(
-            //                continuationToken,
-            //                pageSizeHint,
-            //                cancellationToken)
-            //           .ConfigureAwait(false);
-
-            //        string lastRepository = null;
-            //        if (!string.IsNullOrEmpty(response.Headers.Link))
-            //        {
-            //            Uri nextLink = new Uri(response.Headers.Link);
-            //            NameValueCollection queryParams = HttpUtility.ParseQueryString(nextLink.Query);
-            //            lastRepository = queryParams["last"];
-            //        }
-
-            //        return Page<string>.FromValues(response.Value.RepositoriesValue, lastRepository, response.GetRawResponse());
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        scope.Failed(ex);
-            //        throw;
-            //    }
-            //});
+            });
         }
 
         /// <summary> List repositories. </summary>
