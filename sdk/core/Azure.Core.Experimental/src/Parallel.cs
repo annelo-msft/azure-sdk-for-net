8ca663566d6d71abf25b3a11ac041bf573471cef
Anne Loomis Thompson <annelo@microsoft.com>
2021-06-10 11:38:44 -0700

    API experiment for experimental

---
+++ sdk/core/Azure.Core.Experimental/src/Parallel.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable CA1801 // Review unused parameters
+namespace Azure.Core
+{
+    /// <summary>
+    /// </summary>
+    public static class Parallel
+    {
+        //public static Task ForEachAsync<TSource>(IAsyncEnumerable<TSource> source, ParallelOptions parallelOptions, Func<TSource, CancellationToken, ValueTask> body)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //public static Task ForEachAsync<TSource>(IEnumerable<TSource> source, ParallelOptions parallelOptions, Func<TSource, CancellationToken, ValueTask> body)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public static Task ForEachAsync<TSource>(IAsyncEnumerable<TSource> source, int maxDegreeOfParallelism, Func<TSource, CancellationToken, ValueTask> body)
+        {
+            throw new NotImplementedException();
+        }
+        public static Task ForEachAsync<TSource>(IEnumerable<TSource> source, int maxDegreeOfParallelism, Func<TSource, CancellationToken, ValueTask> body)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public static Task ForEachAsync<TSource>(IEnumerable<TSource> source, CancellationToken cancellationToken, Func<TSource, CancellationToken, ValueTask> body)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //public static Task ForEachAsync<TSource>(IAsyncEnumerable<TSource> source, CancellationToken cancellationToken, Func<TSource, CancellationToken, ValueTask> body)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+#pragma warning restore CA1801 // Review unused parameters
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
