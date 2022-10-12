1d659f9904321a92f4d32114c0df415ca63b816e
Anne Thompson <annelo@microsoft.com>
2022-10-12 14:38:39 -0700

    toy test app

---
+++ sdk/core/Azure.Core/TestApp/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using Azure;
+
+Console.WriteLine("Hello, World!");
+
+dynamic json = JsonData.Parse(new BinaryData(new
+{
+    Foo = new
+    {
+        Bar = "hi"
+    }
+}));
+
+Console.WriteLine(json.Foo);
+Console.WriteLine(json.Foo.Bar);
+Console.WriteLine(json);
+
+//var model = (FooModel)json;
+//Console.WriteLine(model.Bar);
+
+json = JsonData.Parse("5");
+
+Console.WriteLine(((int)json) + 5);
+
+json = JsonData.Parse("[1, 2, 3, 4, 5, 6]");
+
+Console.WriteLine((int)json[5]);
+
+// Cast to custom model: `var model = (FooModel)json.Foo`
+
+
+
+
+Console.ReadLine();
+
+
+public class FooModel
+{
+    public string Bar { get; set; }
+}
+
