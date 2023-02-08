b28487c9a27a5212b7fa47e3c2a0b341ddd691ff
Anne Thompson <annelo@microsoft.com>
2023-02-08 11:33:51 -0800

    Add tests for MutableJsonDocument and RequestContent

---
+++ sdk/core/Azure.Core.Experimental/tests/public/MutableJsonDocumentTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Threading;
+using Azure.Core.Dynamic;
+using NUnit.Framework;
+
+namespace Azure.Core.Experimental.Tests.Public
+{
+    public class MutableJsonDocumentTests
+    {
+        [Test]
+        public void CanCreateRequestContent()
+        {
+            string json = @"
+                {
+                  ""Foo"" : ""foo""
+                }";
+
+            MutableJsonDocument mdoc = MutableJsonDocument.Parse(json);
+
+            mdoc.RootElement.SetProperty("Bar", "bar");
+
+            RequestContent content = RequestContent.Create(mdoc);
+
+            MemoryStream stream = new();
+            content.WriteTo(stream, CancellationToken.None);
+            stream.Flush();
+            stream.Position = 0;
+
+            string value = BinaryData.FromStream(stream).ToString();
+            Assert.IsTrue(value.Contains(@"""Bar"":""bar"""));
+        }
+    }
+}
