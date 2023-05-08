f8a7846edb5108e1ea11423e9880e183a0c279e1
Anne Thompson <annelo@microsoft.com>
2023-05-08 10:13:14 -0700

    Thinking

---
+++ sdk/core/Azure.Core/src/DynamicData/DynamicDataOptions.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json;
+
+namespace Azure.Core.Dynamic
+{
+    internal class DynamicDataOptions
+    {
+        private static readonly JsonSerializerOptions 
+
+        public JsonSerializerOptions SerializeOptions { get; set; }
+        public JsonSerializerDefaults DeserializeOptions { get; set; }
+    }
+}
