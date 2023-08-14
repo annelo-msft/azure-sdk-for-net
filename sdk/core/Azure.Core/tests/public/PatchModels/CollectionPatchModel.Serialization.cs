721c8699c8db8fb8785b6626bdf9dbc47b5e8351
Anne Thompson <annelo@microsoft.com>
2023-08-14 09:41:26 -0700

    Add CollectionPatchModel

---
+++ sdk/core/Azure.Core/tests/public/PatchModels/CollectionPatchModel.Serialization.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Text.Json;
+using Azure.Core.Json;
+using Azure.Core.Serialization;
+
+namespace Azure.Core.Tests.PatchModels
+{
+    public partial class CollectionPatchModel : IModelJsonSerializable<CollectionPatchModel>
+    {
+        CollectionPatchModel IModelJsonSerializable<CollectionPatchModel>.Deserialize(ref Utf8JsonReader reader, ModelSerializerOptions options)
+        {
+            JsonDocument doc = JsonDocument.ParseValue(ref reader);
+            MutableJsonDocument mdoc = new(doc, new JsonSerializerOptions());
+            return new CollectionPatchModel(mdoc.RootElement);
+        }
+
+        CollectionPatchModel IModelSerializable<CollectionPatchModel>.Deserialize(BinaryData data, ModelSerializerOptions options)
+        {
+            MutableJsonDocument mdoc = MutableJsonDocument.Parse(data);
+            return new CollectionPatchModel(mdoc.RootElement);
+        }
+
+        void IModelJsonSerializable<CollectionPatchModel>.Serialize(Utf8JsonWriter writer, ModelSerializerOptions options)
+        {
+            _element.WriteTo(writer, options.Format.ToString());
+        }
+
+        BinaryData IModelSerializable<CollectionPatchModel>.Serialize(ModelSerializerOptions options)
+        {
+            using MemoryStream stream = new();
+            using Utf8JsonWriter writer = new(stream);
+            _element.WriteTo(writer, options.Format.ToString());
+            writer.Flush();
+            stream.Position = 0;
+            return BinaryData.FromStream(stream);
+        }
+    }
+}
---
+++ sdk/core/Azure.Core/tests/public/PatchModels/CollectionPatchModel.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core.Json;
+
+namespace Azure.Core.Tests.PatchModels
+{
+    /// <summary>
+    /// This model illustrates collection properties on Patch models.
+    /// </summary>
+    public partial class CollectionPatchModel
+    {
+#pragma warning disable AZC0020 // Avoid using banned types in libraries
+        private readonly MutableJsonElement _element;
+
+        /// <summary>
+        /// Public constructor.
+        /// </summary>
+        public CollectionPatchModel()
+        {
+            _element = MutableJsonDocument.Parse(MutableJsonDocument.EmptyJson).RootElement;
+        }
+
+        /// <summary>
+        /// Serialization constructor.
+        /// </summary>
+        /// <param name="element"></param>
+        internal CollectionPatchModel(MutableJsonElement element)
+        {
+            _element = element;
+        }
+
+#pragma warning restore AZC0020 // Avoid using banned types in libraries
+    }
+}
